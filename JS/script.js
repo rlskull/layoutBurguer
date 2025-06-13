13c79e051d55b621af1d27c7f1be85188b4efb9e
Rafael <rafaluizsantos@gmail.com>
2025-06-13 10:05:50 -0300

    menu funcionando

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.btnMobile').on('click', function(){
+        $('#mobileMenu').toggleClass('active');
+        $('.btnMobile').find('i').toggleClass("fa-x");
+    });
+});
